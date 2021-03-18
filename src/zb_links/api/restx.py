da6d028b5728cad45a0acfb9272e9718deedc3ad
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-03-18 20:11:13 +0100

    Run linting on source files

--- src/zb_links/api/restx.py
+++ src/zb_links/api/restx.py
@@ -2,10 +2,11 @@
 # Header + Authentication
 # ------------------------------------------------------------------------------
 
-from flask_restx import Api
-from functools import wraps
-from flask import request, url_for
 import os
+from functools import wraps
+
+from flask import request, url_for
+from flask_restx import Api
 
 
 def token_required(f):
@@ -13,9 +14,11 @@ def token_required(f):
     def decorated(*args, **kwargs):
 
         token = None
-        if not 'ZBMATH_API_KEY' in os.environ:
-            return {"message": "Authentication is not implemented on this server. "
-                               "Configure ZBMATH_API_KEY for write operations."}, 501
+        if not "ZBMATH_API_KEY" in os.environ:
+            return {
+                "message": "Authentication is not implemented on this server. "
+                "Configure ZBMATH_API_KEY for write operations."
+            }, 501
 
         if "X-API-KEY" in request.headers:
             token = request.headers["X-API-KEY"]
@@ -23,7 +26,7 @@ def token_required(f):
         if not token:
             return {"message": "Token is missing."}, 401
 
-        if token != os.environ.get('ZBMATH_API_KEY'):
+        if token != os.environ.get("ZBMATH_API_KEY"):
             return {"message": "Token is wrong."}, 401
 
         return f(*args, **kwargs)
@@ -31,24 +34,19 @@ def token_required(f):
     return decorated
 
 
-authorizations = {
-    "apikey": {
-        "type": "apiKey",
-        "in": "header",
-        "name": "X-API-KEY"
-    }
-}
+authorizations = {"apikey": {"type": "apiKey", "in": "header", "name": "X-API-KEY"}}
 
 # Fix for mixed content when deployed on https. Will be removed when resolved.
 # https://github.com/python-restx/flask-restx/issues/188
 class PatchedApi(Api):
     @property
     def specs_url(self):
-        return url_for(self.endpoint('specs'))
+        return url_for(self.endpoint("specs"))
+
 
 api = PatchedApi(
     version="0.1.0",
     title="zbMATH Links API",
     description="Links between zbMATH and selected partners API",
-    authorizations=authorizations
+    authorizations=authorizations,
 )
