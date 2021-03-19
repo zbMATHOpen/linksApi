10c12d6813dbcd33119ed40ec1e2dd3007caf27c
dehsani-academic <dehsani.math@gmail.com>
2021-03-19 14:49:09 +0100

    added drop_all command to clear db

--- src/zb_links/db/manage_db.py
+++ src/zb_links/db/manage_db.py
@@ -6,17 +6,15 @@ from zb_links.db.models import db
 managebp = Blueprint("manage_db", __name__)
 
 
-# clear the database
-@managebp.cli.command("reset")
-def db_reset():
-    connection = db.engine.connect()
-
-    meta = db.metadata
-    for table in reversed(meta.sorted_tables):
-        connection.execute(
-            sql.SQL("DROP TABLE IF EXISTS {};").format(
-                sql.Identifier(table))
-        )
+# clears the database
+# is necessary if a new install is performed 
+# after a previous version had already populated
+# the database
+@managebp.cli.command("drop_all")
+def db_drop_all():
+    
+    db.drop_all()
 
     # drop alembic as well
+    connection = db.engine.connect()
     connection.execute("DROP TABLE IF EXISTS alembic_version;")
