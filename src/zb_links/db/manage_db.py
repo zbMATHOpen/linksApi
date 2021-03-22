9b1a1c99d1d1c1f8abd7271cc7c3b54c96e4a619
dehsani-academic <dehsani.math@gmail.com>
2021-03-22 13:03:02 +0100

    try clause for db drop

--- src/zb_links/db/manage_db.py
+++ src/zb_links/db/manage_db.py
@@ -8,9 +8,11 @@ managebp = Blueprint("manage_db", __name__)
 # clears the database
 @managebp.cli.command("drop_all")
 def db_drop_all():
-    
     db.drop_all()
 
     # drop alembic as well
     connection = db.engine.connect()
-    connection.execute("DROP TABLE IF EXISTS alembic_version;")
+    try:
+        connection.execute("DROP TABLE alembic_version;")
+    except Exception:
+        connection.execute("DROP TABLE IF EXISTS alembic_version;")
