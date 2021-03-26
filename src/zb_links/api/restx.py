bc31baa25b9778c824d8c943f1bc188d945f2dc1
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-03-26 21:32:49 +0100

    Ensure backwards compatible with env var config

--- src/zb_links/api/restx.py
+++ src/zb_links/api/restx.py
@@ -3,29 +3,31 @@
 # ------------------------------------------------------------------------------
 
 import configparser
+import os
 from functools import wraps
 
 from flask import request, url_for
 from flask_restx import Api
 
-
 config = configparser.ConfigParser()
 config.read('config.ini')
+
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
 
         token = None
-        api_key = None
+        api_key: str
+        if "ZBMATH_API_KEY" in os.environ:
+            config.set('key', 'secret_key', os.environ.get("ZBMATH_API_KEY"))
         try:
             api_key = config['key']['secret_key']
         except KeyError:
             return {
-                "message": "Authentication is not implemented on this server. "
-                "Configure ZBMATH_API_KEY for write operations."
-            }, 501
-
+                       "message": "Authentication is not implemented on this server. "
+                                  "Configure ZBMATH_API_KEY for write operations."
+                   }, 501
 
         if "X-API-KEY" in request.headers:
             token = request.headers["X-API-KEY"]
@@ -45,6 +47,7 @@ authorizations = {
     "apikey": {"type": "apiKey", "in": "header", "name": "X-API-KEY"}
 }
 
+
 # Fix for mixed content when deployed on https. Will be removed when resolved.
 # https://github.com/python-restx/flask-restx/issues/188
 class PatchedApi(Api):
@@ -55,7 +58,8 @@ class PatchedApi(Api):
 
 app_settings = config['app']['app_settings']
 db_uri = config['DB']['db_uri']
-
+if "DATABASE_URL" in os.environ:
+    config.set('DB', 'db_uri', os.environ.get('DATABASE_URL'))
 api = PatchedApi(
     version="0.1.0",
     title="zbMATH Links API",
