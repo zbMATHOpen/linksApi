4cdf94cf4e38a8cc4addd59ab281f4655ec1cca2
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-03-27 19:57:05 +0100

    Add common test config

--- tests/api/link/display_test.py
+++ tests/api/link/display_test.py
@@ -1,24 +1,3 @@
-import os
-import time
-
-import pytest
-
-from zb_links.app import create_app
-
-
-@pytest.fixture
-def client():
-    if os.getenv("PYCHARM_HOSTED"):
-        # wait for database migrations to complete
-        time.sleep(5)
-
-    app = create_app()
-    app.config['TESTING'] = True
-
-    with app.test_client() as client:
-        yield client
-
-
 def test_get_display_all(client):
     response = client.get("/links_api/link/")
     data = response.json
