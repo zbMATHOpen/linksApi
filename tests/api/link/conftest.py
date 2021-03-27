61e21c05d4bd4df2f23cd7214e819d830e4ae4dd
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-03-27 20:05:29 +0100

    Add test for year statistics_years
    * Only setup app once

--- tests/api/link/conftest.py
+++ tests/api/link/conftest.py
@@ -5,15 +5,14 @@ import pytest
 
 from zb_links.app import create_app
 
+if os.getenv("PYCHARM_HOSTED"):
+    # wait for database migrations to complete
+    time.sleep(5)
+app = create_app()
+app.config['TESTING'] = True
+
 
 @pytest.fixture
 def client():
-    if os.getenv("PYCHARM_HOSTED"):
-        # wait for database migrations to complete
-        time.sleep(5)
-
-    app = create_app()
-    app.config['TESTING'] = True
-
     with app.test_client() as client:
         yield client
