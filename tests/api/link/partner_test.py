664db9409ddfb8ca61e624af44dd220f44cfaa80
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-03-27 22:56:17 +0100

    Fully covert partners endpoint

--- tests/api/link/partner_test.py
+++ tests/api/link/partner_test.py
@@ -32,6 +32,38 @@ def test_put_partner(client):
     assert test_name == name
 
 
+def test_put_partner_invalid_id(client):
+    headers = {"X-API-KEY": os.getenv("ZBMATH_API_KEY")}
+    test_name = "Test"
+    json = {"partner id": 2, "partner name": test_name}
+    param = urlencode(json)
+    response = client.put(f"/links_api/partner/?{param}",
+                          headers=headers,
+                          )
+    assert response.status_code == 422
+    data = response.json
+    assert "invalid" in data['message'].lower()
+
+
+def test_put_partner_full(client):
+    headers = {"X-API-KEY": os.getenv("ZBMATH_API_KEY")}
+    test_schema = "Test"
+    json = {"partner id": 1,
+            "partner id scheme": test_schema,
+            "partner url": "https://test.te.st"}
+    param = urlencode(json)
+    response = client.put(f"/links_api/partner/?{param}",
+                          headers=headers,
+                          )
+    assert response.status_code == 200
+    data = response.json
+    assert data is None
+    response = client.get("/links_api/partner/")
+    data = response.json
+    schema: dict = data[0].get("scheme")
+    assert test_schema == schema
+
+
 @pytest.mark.skip(reason="currently json input is not supported")
 def test_put_partner_via_json(client):
     headers = {"X-API-KEY": os.getenv("ZBMATH_API_KEY")}
