ddea8d2e5ddd7d8ec759d0850a328962d4fd4160
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-03-27 22:41:21 +0100

    Add test for put partner

--- tests/api/link/conftest.py
+++ tests/api/link/conftest.py
@@ -8,6 +8,7 @@ from zb_links.app import create_app
 if os.getenv("PYCHARM_HOSTED"):
     # wait for database migrations to complete
     time.sleep(5)
+os.environ["ZBMATH_API_KEY"] = "PyTestKey"
 app = create_app()
 app.config['TESTING'] = True
 
--- tests/api/link/partner_test.py
+++ tests/api/link/partner_test.py
@@ -1,3 +1,9 @@
+import os
+from urllib.parse import urlencode
+
+import pytest
+
+
 def test_get_partner(client):
     response = client.get("/links_api/partner/")
     data = response.json
@@ -6,3 +12,33 @@ def test_get_partner(client):
     assert len(entry.keys()) == 4
     url: dict = entry.get("url")
     assert "https" in url
+
+
+# potentially destructive test
+def test_put_partner(client):
+    headers = {"X-API-KEY": os.getenv("ZBMATH_API_KEY")}
+    test_name = "Test"
+    json = {"partner id": 1, "partner name": test_name}
+    param = urlencode(json)
+    response = client.put(f"/links_api/partner/?{param}",
+                          headers=headers,
+                          )
+    data = response.json
+    assert response.status_code == 200
+    assert data is None
+    response = client.get("/links_api/partner/")
+    data = response.json
+    name: dict = data[0].get("name")
+    assert test_name == name
+
+
+@pytest.mark.skip(reason="currently json input is not supported")
+def test_put_partner_via_json(client):
+    headers = {"X-API-KEY": os.getenv("ZBMATH_API_KEY")}
+    json = {"partner_id": 1, "partner_name": "Test name"}
+    response = client.put(f"/links_api/partner/",
+                          headers=headers,
+                          json=json
+                          )
+    data = response.json
+    assert data is None
