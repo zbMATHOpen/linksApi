49dabe0b13ea1c466e4676f46a648378b3ddae90
matteo <matteo@zbmath.org>
2021-05-10 12:33:05 +0200

    small changes

--- src/zb_links/api/link/statistics_msc.py
+++ src/zb_links/api/link/statistics_msc.py
@@ -23,6 +23,6 @@ class MSCCollection(Resource):
     def get():
         """Occurrence of primary 2-digit level MSC codes"""
         queries = ZBTarget.query.all()
-        msc_primary_list = [str(item.msc)[0:2] for item in queries if item.msc]
+        msc_primary_list = [str(item.classification)[0:2] for item in queries if item.msc]
         counter = Counter(msc_primary_list).most_common()
         return counter
