b9c8bd8ba8f84355a3770c9031b347491fd4451f
matteo <matteo@zbmath.org>
2021-05-10 18:21:48 +0200

    first attempt

--- src/zb_links/api/link/statistics_msc.py
+++ src/zb_links/api/link/statistics_msc.py
@@ -3,8 +3,9 @@
 # ------------------------------------------------------------------------------
 
 from collections import Counter
-# from flask import request
-from flask_restx import Resource
+from flask import request
+from zb_links.api.link.display import get_display, link
+from flask_restx import Resource, reqparse
 
 from zb_links.api.restx import api
 from zb_links.db.models import ZBTarget, Link
@@ -15,45 +16,49 @@ ns = api.namespace(
     "of referenced papers in the source",
 )
 
+msc_arguments = reqparse.RequestParser()
+
+msc_arguments.add_argument("partner name", type=str, required=True)
+
 
 # Distribution of MSC codes
-@ns.route("/msc/")
-class MSCCollection(Resource):
-    @staticmethod
-    def get():
-        """Occurrence of primary 2-digit level MSC codes"""
-        queries = ZBTarget.query.\
-            join(Link, Link.document == ZBTarget.id).\
-            filter(Link.type == "DLMF").all()
-        msc_primary_list = [
-            str(item.classification)[0:2]
-            for item in queries
-            if item.classification]
-        counter = Counter(msc_primary_list).most_common()
-        return counter
-
 # @ns.route("/msc/")
 # class MSCCollection(Resource):
-#     # @api.expect(link_item_arguments)
-#     @api.marshal_with(link)
-#     @api.doc(
-#         params={
-#             "partner name": {"description": "Ex: DLMF"},
-#         }
-#     )
-#     def get(self):
+#     @staticmethod
+#     def get():
 #         """Occurrence of primary 2-digit level MSC codes"""
-#         args = request.args
-#         partner_name = args["type"]
-#
 #         queries = ZBTarget.query.\
 #             join(Link, Link.document == ZBTarget.id).\
-#             filter(Link.type == partner_name).\
-#             all()
-#
+#             filter(Link.type == "DLMF").all()
 #         msc_primary_list = [
 #             str(item.classification)[0:2]
 #             for item in queries
 #             if item.classification]
 #         counter = Counter(msc_primary_list).most_common()
 #         return counter
+
+@ns.route("/msc/")
+class MSCCollection(Resource):
+    @api.expect(msc_arguments)
+    @api.marshal_with(link)
+    @api.doc(
+        params={
+            "partner name": {"description": "Ex: DLMF"},
+        }
+    )
+    def get(self):
+        """Occurrence of primary 2-digit level MSC codes"""
+        args = request.args
+        partner_name = args["type"]
+
+        queries = ZBTarget.query.\
+            join(Link, Link.document == ZBTarget.id).\
+            filter_by(partner_name=partner_name).\
+            all()
+
+        msc_primary_list = [
+            str(item.classification)[0:2]
+            for item in queries
+            if item.classification]
+        counter = Counter(msc_primary_list).most_common()
+        return counter
