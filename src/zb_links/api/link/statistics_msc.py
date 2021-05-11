5f230ac814f57e29532e69ec3e0d648e42e220f8
matteo <matteo@zbmath.org>
2021-05-11 11:34:04 +0200

    statistics/msc working (search by partner)

--- src/zb_links/api/link/statistics_msc.py
+++ src/zb_links/api/link/statistics_msc.py
@@ -4,7 +4,6 @@
 
 from collections import Counter
 from flask import request
-from zb_links.api.link.display import get_display, link
 from flask_restx import Resource, reqparse
 
 from zb_links.api.restx import api
@@ -18,43 +17,25 @@ ns = api.namespace(
 
 msc_arguments = reqparse.RequestParser()
 
-msc_arguments.add_argument("partner name", type=str, required=True)
+msc_arguments.add_argument("type", type=str, required=True)
 
-
-# Distribution of MSC codes
-# @ns.route("/msc/")
-# class MSCCollection(Resource):
-#     @staticmethod
-#     def get():
-#         """Occurrence of primary 2-digit level MSC codes"""
-#         queries = ZBTarget.query.\
-#             join(Link, Link.document == ZBTarget.id).\
-#             filter(Link.type == "DLMF").all()
-#         msc_primary_list = [
-#             str(item.classification)[0:2]
-#             for item in queries
-#             if item.classification]
-#         counter = Counter(msc_primary_list).most_common()
-#         return counter
 
 @ns.route("/msc/")
 class MSCCollection(Resource):
     @api.expect(msc_arguments)
-    @api.marshal_with(link)
     @api.doc(
         params={
-            "partner name": {"description": "Ex: DLMF"},
+            "type": {"description": "Ex: DLMF, euclid, eudml, etc."},
         }
     )
     def get(self):
-        """Occurrence of primary 2-digit level MSC codes"""
+        """Occurrence of primary 2-digit level MSC codes in each partner"""
         args = request.args
         partner_name = args["type"]
 
         queries = ZBTarget.query.\
             join(Link, Link.document == ZBTarget.id).\
-            filter_by(partner_name=partner_name).\
-            all()
+            filter(Link.type == partner_name).all()
 
         msc_primary_list = [
             str(item.classification)[0:2]
