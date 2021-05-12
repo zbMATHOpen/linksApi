997a243c966abae9a6a75d11888aad9d2dbf3cc2
dariush <dariush@zbmath.org>
2021-05-12 11:18:45 +0200

    removed tests

--- tests/api/link/display_test.py
+++
@@ -1,8 +0,0 @@
-def test_get_display_all(client):
-    response = client.get("/links_api/link/")
-    data = response.json
-    assert len(data) == 1
-    entry: dict = data[0]
-    assert len(entry.keys()) == 5
-    src: dict = entry.get("Source")
-    assert len(src) == 4
