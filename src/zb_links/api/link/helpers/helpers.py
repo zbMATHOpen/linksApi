dddaf9cfa893aa670f4406bcc395955e42f81636
dariush <dariush@zbmath.org>
2021-05-28 12:53:59 +0200

    post link edited; test added

--- src/zb_links/api/link/helpers/helpers.py
+++ src/zb_links/api/link/helpers/helpers.py
@@ -22,3 +22,16 @@ def make_message(status_code, message):
     response = jsonify({"status": status_code, "message": message})
     response.status_code = status_code
     return response
+
+
+def get_provider():
+    """
+    Returns
+    -------
+    provider_name : str
+        the currently logged in user who is providing the new link
+
+    """
+
+    provider_name = "Dariush, Matteo"
+    return provider_name
