6940cc1277d727cb998603fcf9a276dd179910b5
dariush <dariush@zbmath.org>
2021-07-16 11:18:53 +0200

    get title fixture for tests

--- tests/api/link/conftest.py
+++ tests/api/link/conftest.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import pytest
+from unittest import mock
 
 from zb_links.app import create_app
 
@@ -17,3 +18,13 @@ app.config['TESTING'] = True
 def client():
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture(autouse=True)
+def mock_title():
+    with mock.patch(
+            "zb_links.api.link.helpers.dlmf_source_helpers.get_title"
+        ) as title_mock:
+
+        title_mock.return_value = "Asymptotic Approximation"
+        yield title_mock
