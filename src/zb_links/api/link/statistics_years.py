3605a4fa5809019906441f82b8eccbdfdf28490c
matteo <matteo@zbmath.org>
2021-08-23 14:21:12 +0200

    new descriptions shortened

--- src/zb_links/api/link/statistics_years.py
+++ src/zb_links/api/link/statistics_years.py
@@ -26,7 +26,7 @@ class YearCollection(Resource):
         }
     )
     def get(self):
-        """Occurrence of years of publication of zbMATH objects for a given partner"""
+        """Occurrence of years of publication of zbMATH objects"""
         args = request.args
         partner_name = args["partner"]
 
